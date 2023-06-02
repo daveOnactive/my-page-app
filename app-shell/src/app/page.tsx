1762a900b4cf44f3af5445cba5585e06c767c94a
daveOnactive <chinweike89@gmail.com>
2023-06-02 12:16:19 +0100

    updated page file in app-shell

--- app-shell/src/app/page.tsx
+++ app-shell/src/app/page.tsx
@@ -1,8 +1,10 @@
 
     export default function Home() {
-      <main>
-        <h1>Testing component builder</h1>
-        <p>What an interesting time to be alive, thank you everlasting God.</p>
-      </main>
+     return (
+        <main>
+          <h1>Testing component builder</h1>
+          <p>What an interesting time to be alive, thank you everlasting God.</p>
+        </main>
+      );
     }
   
