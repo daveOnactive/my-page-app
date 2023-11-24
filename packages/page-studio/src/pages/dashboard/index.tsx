1665ec9c7031f5b9819b170b44450e1598c41482
Dave <david.chinweike@sastrify.com>
2023-11-24 18:43:05 +0100

    pagestudio-user-dashboard

--- packages/page-studio/src/pages/dashboard/index.tsx
+++ packages/page-studio/src/pages/dashboard/index.tsx
@@ -1,5 +1,42 @@
+import { Box, ProjectItem } from '@my-page/design-system';
+import { Header } from './features';
+
+
+const projectItem = [
+  {
+    name: 'Busy Shop',
+    url: 'busyshop.swiftpage.com',
+  },
+  {
+    name: 'Beauty Shop',
+    url: 'beautyshop.swiftpage.com',
+  },
+  {
+    name: 'Maximum Biz',
+    url: 'maximumsbiz.swiftpage.com',
+  }
+]
+
 export const Dashboard = () => {
   return (
-    <>This is dashboard</>
+    <Box width='100%'>
+      <Header
+        projectCount={projectItem.length}
+      />
+      <Box>
+        {
+          projectItem.map((item) => (
+            <Box my={4}>
+              <ProjectItem
+                projectName={item.name}
+                projectUrl={item.url}
+                goToProject={() => null}
+                key={item.name}
+              />
+            </Box>
+          ))
+        }
+      </Box>
+    </Box>
   )
 }
